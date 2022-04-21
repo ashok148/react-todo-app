ad1f447e738b2a063dc6f84d68f2e24d62de6a1b
Ashok Kumar <59107244+ashok148@users.noreply.github.com>
2022-04-21 18:53:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,11 @@
-import React, { useState } from "react";
-import Todo from './components/Todo';
-import TodoForm from './components/TodoForm';
-import "./App.css";
-
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      text: "Learn about React",
-      isCompleted: false
-    },
-    {
-      text: "Meet friends",
-      isCompleted: false
-    },
-    {
-      text: "Build todo app",
-      isCompleted: false
-    }
-  ]);
-
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  };
-
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = !newTodos[index].isCompleted;
-    setTodos(newTodos);
-  };
-
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
   return (
-    <div className="app">
-      <div className="todo-list">
-        <h1 className="title">React todo Application</h1>
-        {todos.map((todo, index) => (
-          <Todo
-            key={index}
-            index={index}
-            todo={todo}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
-          />
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
